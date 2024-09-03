34a97e3504ed7708cc428c4fe060462e922658ad
rphldc <raphaelcamara.rc@gmail.com>
2024-09-03 02:13:03 -0300

    improvements in views and urls, addition of dynamic paths in htmls and creation of a "model" using faker for testing

--- recipes/views.py
+++ recipes/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
+from utils.recipes.main import make_recipe
 
 
 def home(request):
-    return render(request, 'recipes/pages/home.html', context={'name': 'Raphael Dias Câmara'})
+    return render(request, 'recipes/pages/home.html', context={'recipes': make_recipe() for _ in range(10)})
 
 
 def recipes(request, id):
-    return render(request, 'recipes/pages/recipe-view.html', context={'name': 'Raphael Dias Câmara'})
+    return render(request, 'recipes/pages/recipe-view.html', context={'recipe': make_recipe()})
